692e424dbf9decf0f6472427be06a35c42207643
44304001 <44304001@L815WD150061.edu.cbe.ab.ca>
2022-03-14 10:21:22 -0600

    Updated 10:21AM

--- Chapter8/src/Account/BusinessAccount.java
+++ Chapter8/src/Account/BusinessAccount.java
@@ -1,5 +1,12 @@
 package Account;
 
-public class BusinessAccount {
+public class BusinessAccount extends Customer{
+	
+	String Business;
+	
+	public BusinessAccount(String fName, String lName) 
+	{
+		super(fName,lName);
+	}
 
 }
--- Chapter8/src/Account/PersonalAccount.java
+++ Chapter8/src/Account/PersonalAccount.java
@@ -1,5 +1,13 @@
 package Account;
 
-public class PersonalAccount {
+public class PersonalAccount extends Customer{
+	
+	String Personal;
+	
+	public PersonalAccount(String fName, String lName) 
+	{
+		super(fName,lName);
+	}
 
 }
+
