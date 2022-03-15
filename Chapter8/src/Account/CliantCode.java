8acac2264249fe7f59969de2defd1fe398b0e12b
44304001 <44304001@L815WD150061.edu.cbe.ab.ca>
2022-03-15 10:59:31 -0600

    Updated 10:59AM

---
+++ Chapter8/src/Account/CliantCode.java
@@ -0,0 +1,18 @@
+package Account;
+
+import java.util.Scanner;
+
+public class CliantCode {
+	
+	public static Customer createcustomer()
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter first name: ");
+		String fName = input.next();
+		
+		System.out.println("Enter last name: ");
+		String lName = input.next();
+		
+	}
+}
