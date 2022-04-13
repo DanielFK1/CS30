bce356ab7bca408835d073cc07e68df587c956de
44304001 <44304001@L815WD220023.edu.cbe.ab.ca>
2022-04-13 10:16:10 -0600

    Updated 10:16AM

--- Chapter11/src/FindAndReplace/Filename.java
+++ Chapter11/src/FindAndReplace/Filename.java
@@ -1,15 +1,18 @@
 package FindAndReplace;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Filename {
 	
 	public static void main(String[] args) 
 	{
-		
+		File FindandReplace = new File("ReplaceandFind.txt");
 		try {
-			File FindandReplace = new File("ReplaceandFind.txt");
 			Scanner Find = new Scanner(FindandReplace);
-			}
+		} catch (FileNotFoundException e) {
+			System.out.print("An error has occurred");
+			e.printStackTrace();
+		}
 	}
 }
