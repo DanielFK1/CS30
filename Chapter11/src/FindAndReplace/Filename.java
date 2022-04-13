122f057ac5bb7514cc8b0846f763e30456aef797
44304001 <44304001@L815WD220023.edu.cbe.ab.ca>
2022-04-13 10:17:34 -0600

    Updated 10:17AM

--- Chapter11/src/FindAndReplace/Filename.java
+++ Chapter11/src/FindAndReplace/Filename.java
@@ -8,9 +8,13 @@ public class Filename {
 	public static void main(String[] args) 
 	{
 		File FindandReplace = new File("ReplaceandFind.txt");
-		try {
+		try 
+		{
 			Scanner Find = new Scanner(FindandReplace);
-		} catch (FileNotFoundException e) {
+		} 
+		
+		catch (FileNotFoundException e) 
+		{
 			System.out.print("An error has occurred");
 			e.printStackTrace();
 		}
