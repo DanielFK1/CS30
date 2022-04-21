f1748eb0297b7df75c4f0236179dd1a3d19ece05
44304001 <44304001@L815WD220023.edu.cbe.ab.ca>
2022-04-21 11:44:40 -0600

    Updated 11:44AM

--- Chapter11/src/FindAndReplace/Roster.java
+++ Chapter11/src/FindAndReplace/Roster.java
@@ -7,27 +7,37 @@ public class Roster {
 	
 	public static void main(String[] args) 
 	{
-		File stuname = new File ("studentsname.txt");
+		File stuname; 
+		Scanner input = new Scanner(System.in);
+		//Ask the file name
+			System.out.println("What is the file name:");
+			String file = input.next();
+			
+			File stuname = new File(file + ".txt");
+		
+		//Ask how many students
+			System.out.println("How many students are in the class:");
+			int numstu = input.nextInt();
+		
+		
 		
 		try {
 			FileOutputStream out = new FileOutputStream(stuname);
 			ObjectOutputStream writestu = new ObjectOutputStream(out);
 			
-			Scanner input = new Scanner(System.in);
+			//for loop required
 			
-			System.out.println("What is the students first name:");
-			String firstname = input.nextLine();
-			
-			System.out.println("What is the students last name:");
-			String lastname = input.nextLine();	
-			String name = (firstname + lastname);
-			
-			
-			System.out.println("How many students are in the class:");
-			int numstu = input.nextInt();
-			
-			writestu.writeObject(name);
-			writestu.close();
+				System.out.println("What is the students first name:");
+				String firstname = input.next();
+				
+				System.out.println("What is the students last name:");
+				String lastname = input.next();	
+				
+				writestu.writeObject(new Stu);
+				System.out.println(name);
+				writestu.close(); 
+				
+				}
 			
 			}
 			
@@ -35,9 +45,6 @@ public class Roster {
 			{
 				System.out.println("File does not exist or could not be found.");
 				System.err.println("FileNotFoundException: " + e.getMessage());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 	}
 }
---
+++ Chapter11/src/FindAndReplace/StuName.java
@@ -0,0 +1,16 @@
+package FindAndReplace;
+
+public class StuName implements Serializable 
+{
+	//declare private 
+	public void Roster(String fName, String lName) {
+		
+		
+	}
+	
+	//create the constructor for the two names
+	
+	
+	//create the toString for the first and last name
+	
+}
