3b889e06934f241bcfa591aa6901357f8ecf30b4
44304001 <44304001@L815WD220023.edu.cbe.ab.ca>
2022-05-05 11:14:04 -0600

    Updated 11:14AM

--- Chapter9/src/arrays/Squares.java
+++ Chapter9/src/arrays/Squares.java
@@ -1,5 +1,18 @@
 package arrays;
 
-public class Squares {
+import java.util.Scanner;
 
+public class Squares {
+	public static void main(String[] args)
+	{
+	
+	Scanner input = new Scanner(System.in);
+	int[] squares = new int[5];
+	for (int i = 0; i < squares.length; i++)
+		{
+			squares[i] = i * i;
+			System.out.print(squares[i] + "\n");
+		}
+
+	}
 }
