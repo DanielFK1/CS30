395d306753bdef083abff404a13f83ec4c1ac88a
44304001 <44304001@L815WD220023.edu.cbe.ab.ca>
2022-05-05 11:53:35 -0600

    Updated 11:53AM

--- Chapter9/src/arrays/FindName.java
+++ Chapter9/src/arrays/FindName.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class FindName {
 	public static int linear(String[] array, String nametoFind)
 	{
+		int numname;
+		String names;
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("How many names are you entering: ");
-		int numname = input.nextInt();
+		numname = input.nextInt();
+		
+		String[] Findname = new String[numname]; 
 		
 		for(int i = 0; i < numname; i++)
 		{
 			System.out.println("What are the names: ");
-			String names = input.next();
+			Findname[i] = input.next();
 		}
 		
+		System.out.println("Who's name do you want to search: ");
+		String searchname = input.next();
 		
 		
 		return 0;
