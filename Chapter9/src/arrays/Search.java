28826bc67334bda4715a9ae03883712a2ed3cc00
44304001 <44304001@L815WD220023.edu.cbe.ab.ca>
2022-05-17 10:43:32 -0600

    Updated 10:43AM

--- Chapter9/src/arrays/Search.java
+++ Chapter9/src/arrays/Search.java
@@ -9,20 +9,21 @@ public class Search {
 			{
 				return(index);
 			} 
+			else {
+				while ((array[index] != nametofind) && (index < array.length - 1))
+				{
+					index += 1;
+				}
+				
+				if (array[index].equals(nametofind)) 
+				{
+					return(index);
+				} 
 			
-			while ((array[index] != nametofind) && (index < array.length - 1))
-			{
-				index += 1;
-			}
-			
-			if (array[index].equals(nametofind)) 
-			{
-				return(index);
-			} 
-			
-			else 
-			{
-				return(-1);
+				else 
+				{
+					return(-1);
+				}
 			}
 		}
 	}
