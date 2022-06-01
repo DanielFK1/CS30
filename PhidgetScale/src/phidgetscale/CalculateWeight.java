3c74418d31068268b900f88f4d7997786c101f98
44304001 <44304001@L815WD220023.edu.cbe.ab.ca>
2022-06-01 10:21:21 -0600

    Updated 10:21AM

--- PhidgetScale/src/phidgetscale/CalculateWeight.java
+++ PhidgetScale/src/phidgetscale/CalculateWeight.java
@@ -1,11 +1,11 @@
 package phidgetscale;
-
 import com.phidget22.*;
 
-public class ScaleOffset 
+public class CalculateWeight 
 {
 	    public static void main(String[] args) throws Exception
 	    {
+
 	        //Create
 	        VoltageRatioInput scale = new VoltageRatioInput();
 	        scale.setHubPort(0);
@@ -13,13 +13,20 @@ public class ScaleOffset
 	        //Open
 	        scale.open(1000);
 	        
+	        //Use your Offset Value
+	        double offsetValue = INSERT VALUE HERE
+	        
 	        //Use your Phidgets
 	        while(true)
 	        {
-	            System.out.println("Offset Value: " + scale.getVoltageRatio());
+	            //Calculate Weight (kg)
+	            double weight = 4700 * (scale.getVoltageRatio() - offsetValue);
+	            
+	            //Display Weight
+	            System.out.println(String.format("%.3f kg", weight));      
+	            
 	            Thread.sleep(250);
 	        }
 	    }    
-	
-} 
-
+}
+	  
---
+++ PhidgetScale/src/phidgetscale/OffsetScale.java
@@ -0,0 +1,25 @@
+package phidgetscale;
+import com.phidget22.*;
+
+public class OffsetScale 
+{
+	public static void main(String[] args) throws Exception
+	{
+	        //Create
+	        VoltageRatioInput scale = new VoltageRatioInput();
+	        scale.setHubPort(0);
+	        
+	        
+	        //Open
+	        //scale.open(1000);
+	        
+	        //Use your Phidgets
+	        while(true)
+	        {
+	            System.out.println("Offset Value: " + scale.getVoltageRatio());
+	            Thread.sleep(250);
+	        }
+	 }    
+}
+	  
+
